c63ba1e6fa242f8fe8286e19d1af6cf8e427764f
berezzin <biarezzin@gmail.com>
2023-05-10 21:33:28 +0200

    Added dependency injection in add_transaction method

--- src/app.py
+++ src/app.py
@@ -1,20 +1,13 @@
 import uvicorn
 from fastapi import APIRouter
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI
 
-from .auth.models import User
-from .auth.router import router as auth_router, current_user
+from .auth.router import router as auth_router
 from .transactions.router import router as transaction_router
 
 app = FastAPI(title='Money API')
 
 main_router = APIRouter()
-
-
-@main_router.get("/protected-route")
-async def protected_route(user: User = Depends(current_user)):
-    return f"Hello, {user.username}"
-
 
 main_router.include_router(auth_router, prefix='/auth', tags=['Auth'])
 main_router.include_router(transaction_router, prefix='/transaction', tags=['Transactions'])
--- src/transactions/router.py
+++ src/transactions/router.py
@@ -1,16 +1,18 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 
 from src.database import async_session
 from src.transactions.models import Transaction
 from .schemas import TransactionSchema
+from ..auth.models import User
+from ..auth.router import current_user
 
 router = APIRouter()
 
 
 @router.post('/add')
-async def add_transaction(transaction_data: TransactionSchema):
+async def add_transaction(transaction_data: TransactionSchema, user: User = Depends(current_user)):
     async with async_session() as session:
-        new_transaction = Transaction(user_id=transaction_data.user_id,
+        new_transaction = Transaction(user_id=user.id,
                                       transaction_comment=transaction_data.transaction_comment,
                                       transaction_amount=transaction_data.transaction_amount,
                                       transaction_category=transaction_data.transaction_category)
