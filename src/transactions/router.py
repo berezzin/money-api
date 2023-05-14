d7308c531fe276c7a9f81c9934c5dd83d1502d63
berezzin <biarezzin@gmail.com>
2023-05-14 21:39:05 +0200

    Added dependency injection in add_transaction router

--- src/transactions/router.py
+++ src/transactions/router.py
@@ -1,9 +1,11 @@
 import time
+from typing import AsyncGenerator
 
 from fastapi import APIRouter, Depends
 from fastapi_cache.decorator import cache
+from sqlalchemy.ext.asyncio import AsyncSession
 
-from src.database import async_session
+from src.database import get_async_session
 from src.transactions.models import Transaction
 from .schemas import TransactionSchema
 from ..auth.models import User
@@ -13,15 +15,16 @@ router = APIRouter()
 
 
 @router.post('/add')
-async def add_transaction(transaction_data: TransactionSchema, user: User = Depends(current_user)):
-    async with async_session() as session:
-        new_transaction = Transaction(user_id=user.id,
-                                      transaction_comment=transaction_data.transaction_comment,
-                                      transaction_amount=transaction_data.transaction_amount,
-                                      transaction_category=transaction_data.transaction_category)
-        session.add(new_transaction)
-        await session.commit()
-        return {'Status': 200}
+async def add_transaction(transaction_data: TransactionSchema,
+                          user: User = Depends(current_user),
+                          session: AsyncSession = Depends(get_async_session)):
+    new_transaction = Transaction(user_id=user.id,
+                                  transaction_comment=transaction_data.transaction_comment,
+                                  transaction_amount=transaction_data.transaction_amount,
+                                  transaction_category=transaction_data.transaction_category)
+    session.add(new_transaction)
+    await session.commit()
+    return {'Status': 200}
 
 
 @router.get('/')
