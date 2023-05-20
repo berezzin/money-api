5e569ca2deaa5ae7194fd5d62d8779a80514eedf
berezzin <biarezzin@gmail.com>
2023-05-20 20:53:54 +0200

    Fix bug with mapping to Transaction class from User class

--- src/auth/models.py
+++ src/auth/models.py
@@ -1,8 +1,11 @@
+from typing import List
+
 from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTableUUID
 from sqlalchemy import Column, String, TIMESTAMP, Numeric, text
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship, Mapped
 
 from src.database import Base
+from src.transactions.models import Transaction
 
 
 class User(SQLAlchemyBaseUserTableUUID, Base):
@@ -12,4 +15,4 @@ class User(SQLAlchemyBaseUserTableUUID, Base):
     join_date = Column(TIMESTAMP, server_default=text('CURRENT_TIMESTAMP'))
     balance = Column(Numeric(precision=10, scale=3, asdecimal=False), nullable=False, server_default='0')
 
-    transactions = relationship('Transaction', cascade='all, delete-orphan')
+    transactions: Mapped[List[Transaction]] = relationship('Transaction', cascade='all, delete-orphan')
