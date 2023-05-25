ad961cb879bccc35b747b2c2e946de253e6f2b35
berezzin <biarezzin@gmail.com>
2023-05-25 20:50:24 +0200

    Added get transactions by category id router

--- src/transactions/router.py
+++ src/transactions/router.py
@@ -2,6 +2,7 @@ import time
 
 from fastapi import APIRouter, Depends
 from fastapi_cache.decorator import cache
+from sqlalchemy import select, and_
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.database import get_async_session
@@ -26,7 +27,19 @@ async def add_transaction(transaction_data: TransactionSchema,
     return {'Status': 200}
 
 
-@router.get('/')
+@router.get('')
+async def get_transactions_by_category_id(category_id: int,
+                                          user: User = Depends(current_user),
+                                          session: AsyncSession = Depends(get_async_session)):
+    query = select(Transaction).where(and_(Transaction.user_id == user.id,
+                                           Transaction.transaction_category == category_id))
+    results = await session.scalars(query)
+    return {'Status': 200,
+            'data': results.all(),
+            'details': None}
+
+
+@router.get('/cached')
 @cache(expire=5)
 def long_cached_operation():
     time.sleep(2)
